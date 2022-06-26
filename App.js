73ded5bf04bd4204bfe56522bbf239b997e87918
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2022-06-26 11:08:07 +0200

    add header and styles

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {
   SafeAreaView,
@@ -29,41 +29,48 @@ const App = () => {
 
   const [refreshing, setRefreshing] = useState(false)
   const [rates, setRates] = useState([])
+  const [date, setDate] = useState(Date.now())
 
   useEffect(() => {
     getDataFromAPI()
-  },[])
+  }, [])
 
   const getDataFromAPI = async () => {
-    setRefreshing(true) 
-    let res = await fetch('http://api.nbp.pl/api/exchangerates/tables/a/?format=json')
+    setRefreshing(true)
+    let res = await fetch('http://api.nbp.pl/api/exchangerates/tables/b/?format=json')
     let json = await res.json();
-    console.log(json[0].rates[0])
+    // console.log(json[0].rates[0])
+    setDate(json[0].effectiveDate)
     setRates(json[0].rates)
     setRefreshing(false)
   }
 
-  const ItemView = ({item}) => {
+  const ItemView = ({ item }) => {
     return (
-      <Text
-
-      >
-        {item.currency} {item.mid}
-      </Text>
+      <View style={styles.itemView}>
+        <Text style={styles.currencyName}>
+          {item.currency}
+        </Text>
+        <Text style={styles.currencyValue}>
+          {item.mid.toFixed(2)} zł
+        </Text>
+      </View>
     )
   }
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <Text style={styles.textLabel}>Kursy walut</Text>
+      <Text style={styles.textLabelSmall}>z dnia {date}</Text>
       {/* {refreshing ? <ActivityIndicator /> : null} */}
       <FlatList
         data={rates}
         keyExtractor={(item) => item.code}
         renderItem={ItemView}
         refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={getDataFromAPI}/>
+          <RefreshControl refreshing={refreshing} onRefresh={getDataFromAPI} />
         }
+        style={styles.flatList}
       />
     </SafeAreaView>
   );
@@ -76,6 +83,31 @@ const styles = StyleSheet.create({
   textLabel: {
     textAlign: 'center',
     fontSize: 26,
+  },
+  textLabelSmall: {
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  flatList: {
+    borderTopWidth: 1,
+  },
+  itemView: {
+    flexDirection: 'row',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  currencyName: {
+    flex: 3,
+    fontSize: 16,
+    padding: 10,
+    borderRightWidth: 1,
+    borderRightColor: '#ccc',
+  },
+  currencyValue: {
+    flex: 1,
+    textAlign: 'center',
+    fontSize: 16,
+    padding: 10,
   },
 });
 
